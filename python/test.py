5796a54d10eb3baebda51e3420a818a251406a5c
Chris Willoughby <chrrrisw@gmail.com>
2015-11-11 13:53:58 +1030

    Test more stuff in python

--- python/test.py
+++ python/test.py
@@ -1,28 +1,24 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from QHexEdit import QHexEdit, QHexEditData
-
-
-class HexEdit(QHexEdit):
-
-    def __init__(self, fileName=None):
-        super(HexEdit, self).__init__()
-        file = open(fileName)
-        data = file.read()
-        self.setData(data)
-        self.setReadOnly(False)
-
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
-    # QHexEditData* hexeditdata = QHexEditData::fromFile("data.bin");
-    data = QHexEditData.fromFile('test.py')
+    # QHexEditData* hexeditdata = QHexEditData::fromFile("test.py");
+    hexeditdata = QHexEditData.fromFile('test.py')
 
     # QHexEdit* hexedit = new QHexEdit();
     # hexedit->setData(hexeditdata);
-    mainWin = QHexEdit()
-    mainWin.setData(data)
+    hexedit = QHexEdit()
+    hexedit.setData(hexeditdata)
 
-    mainWin.show()
+    hexedit.show()
+
+    # hexedit->commentRange(0, 12, "I'm a comment!");
+    hexedit.commentRange(0, 12, "I'm a comment!")
+
+    # hexedit->highlightBackground(0, 10, QColor(Qt::Red));
+    hexedit.highlightBackground(0, 10, QtGui.QColor(255, 0, 0))
+
     sys.exit(app.exec_())
